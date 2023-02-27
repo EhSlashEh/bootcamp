2d4f39dbcae6f59fdc6fd7c3dc35602a52dce185
EhSlashEh <anthonyalanb@hotmail.com>
2023-02-27 18:31:46 -0400

    Update 2 Roman Numeral Converter.js

--- 2 JavaScript Algorithms and Data Structures/2 Roman Numeral Converter.js
+++ 2 JavaScript Algorithms and Data Structures/2 Roman Numeral Converter.js
@@ -1,31 +1,37 @@
 function convertToRoman(num) {
+  console.log("Number given: " + num);
 
-    var tempNum = 0;
-    var i = 0;
+  let romanResult = "";
 
-    while (i < num.length) {
-        print(tempNum);
+  /* Set up dictionary of Roman numbers  */
+  const romanNumeral = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+  };
 
-        if (num[i] == "I") {
-            if (i < num.length - 1 & num[i + 1] == "V") {
-                tempNum += 4;
-                i = i + 1;
-                console.log("IV");
-            }
-            else if((i < (num.length - 1)) & num[i + 1] == "X") {
-                tempNum += 9;
-                i = i + 1;
-                console.log("IX");
-            }
-            else {
-                tempNum += 1;
-                console.log("I");
-            }
-        }
+  /* Check each usable numeral  */
+  for (let romanKey in romanNumeral) {
+
+    /* While there is still value to add, use the biggest roman numeral  */
+    while (num >= romanNumeral[romanKey]) {
+      romanResult += romanKey;
+      num -= romanNumeral[romanKey];
+      console.log("Running result: " + romanResult);
+      console.log("Value left: " + num);
     }
+  }
+
+  console.log("String giving: " + romanResult);
+  return romanResult;
 }
-
-convertToRoman(36);
-
-
-
