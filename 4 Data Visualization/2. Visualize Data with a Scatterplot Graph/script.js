2a98156662e4fd91ef76b7f0d06e76d27c5655f1
EhSlashEh <anthonyalanb@hotmail.com>
2023-03-25 00:27:19 -0300

    Working on scatter

--- 4 Data Visualization/2. Visualize Data with a Scatterplot Graph/script.js
+++ 4 Data Visualization/2. Visualize Data with a Scatterplot Graph/script.js
@@ -1,4 +1,3 @@
-// html loaded
 document.addEventListener('DOMContentLoaded', function () {
 
     // Get the cyclist data
@@ -6,51 +5,79 @@ document.addEventListener('DOMContentLoaded', function () {
         .then(response => response.json())
         .then(data => {
 
-            // use a div
-            const canvas = document.getElementById('content');
-            canvas.width = 800;
-            canvas.height = 600;
-
-            // Get a 2D drawing context
-            const context = canvas.getContext('2d');
+            // use the SVG element
+            const svg = d3.select('#content');
+            svg.attr('width', 800)
+                .attr('height', 600);
 
             // Define dimensions and margins
             const margin = { top: 20, right: 20, bottom: 50, left: 70 };
-            const width = canvas.width - margin.left - margin.right;
-            const height = canvas.height - margin.top - margin.bottom;
+            const width = svg.attr('width') - margin.left - margin.right;
+            const height = svg.attr('height') - margin.top - margin.bottom;
 
             // Define scales for x and y-axes
             const xScale = d3.scaleLinear()
-                .domain([d3.min(data, d => d.Year) - 1, d3.max(data, d => d.Year) + 1])
+                .domain([d3.min(data, dataPoint => dataPoint.Year) - 1, d3.max(data, dataPoint => dataPoint.Year) + 1])
                 .range([0, width]);
             const yScale = d3.scaleLinear()
-                .domain([d3.max(data, d => d.Seconds), d3.min(data, d => d.Seconds)])
+                .domain([d3.max(data, dataPoint => dataPoint.Seconds), d3.min(data, dataPoint => dataPoint.Seconds)])
                 .range([0, height]);
 
+            // Create a tooltip element for dots
+            const tooltip = d3.select('body')
+                .append('div')
+                .attr('id', 'tooltip')
+                .style('opacity', 0);
+
             // Create circles for each data point
-            const circles = d3.select(canvas)
-                .selectAll('circle')
+            const circles = svg.selectAll('circle')
                 .data(data)
                 .enter()
                 .append('circle')
-                .attr('cx', d => xScale(d.Year))
-                .attr('cy', d => yScale(d.Seconds))
+                .attr('cx', d => xScale(d.Year) + margin.left)
+                .attr('cy', d => yScale(d.Seconds) + margin.top)
                 .attr('r', 6)
-                .attr('fill', d => d.Doping ? 'red' : 'steelblue');
+                .attr("class", "dot")
+                .attr("data-xvalue", d => d.Year)
+                .attr("data-yvalue", d => new Date(1970, 0, 1, 0, 0, d.Time))
+                .attr('fill', d => d.Doping ? 'red' : 'steelblue')
+
+                .on("mouseover", function (event, d) {
+                    // Show tooltip
+                    tooltip.transition()
+                        .duration(200)
+                        .style("opacity", .9);
+                    // Update tooltip content
+                    tooltip.html(`${d.Name}<br/>${d.Nationality}<br/>Year: ${d.Year}, Time: ${d.Time}<br/>${d.Doping}`)
+                })
+                .on("mousemove", function (event, d) {
+                    // Update tooltip position
+                    tooltip.style("left", (event.pageX + 10) + "px")
+                        .style("top", (event.pageY - 70) + "px")
+                        .style("position", "absolute");
+                })
+                .on("mouseout", function (d) {
+                    // Hide tooltip
+                    tooltip.transition()
+                        .duration(500)
+                        .style("opacity", 0);
+                });
 
             // Define x and y-axes
-            const xAxis = d3.axisBottom(xScale);
-            const yAxis = d3.axisLeft(yScale);
-
+            const xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"));
+            const yAxis = d3.axisLeft(yScale).tickFormat(d => {
+                const date = new Date(1970, 0, 1, 0, 0, d);
+                return d3.timeFormat('%M:%S')(date);
+            });
             // Add the x and y-axes to the scatterplot
-            d3.select(canvas)
-                .append("g")
+            svg.append("g")
                 .attr("transform", `translate(${margin.left}, ${height + margin.top})`)
+                .attr("id", "x-axis")
                 .call(xAxis);
 
-            d3.select(canvas)
-                .append("g")
+            svg.append("g")
                 .attr("transform", `translate(${margin.left}, ${margin.top})`)
+                .attr("id", "y-axis")
                 .call(yAxis);
         });
 });
