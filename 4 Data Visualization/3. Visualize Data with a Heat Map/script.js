64cd645aee9deac9d481b3b931b15875dc0f9106
EhSlashEh <anthonyalanb@hotmail.com>
2023-03-27 22:36:26 -0300

    Working on heatmap

--- 4 Data Visualization/3. Visualize Data with a Heat Map/script.js
+++ 4 Data Visualization/3. Visualize Data with a Heat Map/script.js
@@ -4,17 +4,15 @@ document.addEventListener('DOMContentLoaded', function () {
     fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json')
         .then(response => response.json())
         .then(data => {
-            // set consts
             const dataset = data.monthlyVariance;
             const years = dataset.map(d => d.year);
-            const months = dataset.map(d => d.month - 1); // Subtract 1 to convert to 0-indexed
+            const months = dataset.map(d => d.month - 1) // Subtract 1 to convert to 0-indexed
+                .filter(m => m >= 0 && m <= 11); // Only keep months between 0 and 11
             const temperatures = dataset.map(d => d.variance);
 
-            // calc max and min temps
             const minTemp = Math.min(...temperatures);
             const maxTemp = Math.max(...temperatures);
 
-            // use the SVG element
             const width = 1200;
             const height = 500;
             const padding = 60;
@@ -22,27 +20,22 @@ document.addEventListener('DOMContentLoaded', function () {
                 .attr('width', width)
                 .attr('height', height);
 
-            // color scale
             const colorScale = d3.scaleSequential()
                 .interpolator(d3.interpolateRdYlBu)
                 .domain([maxTemp, minTemp]);
 
-            // Axis Scale
             const xScale = d3.scaleTime()
                 .domain([new Date(d3.min(years), 0), new Date(d3.max(years), 0)])
-                .range([padding, width ]);
+                .range([padding, width]);
+            let monthsArr = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
             const yScale = d3.scaleBand()
-                .domain(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
+                .domain(monthsArr)
                 .range([height - padding + 6, padding]);
 
-            // Create a tooltip element for dots
             const tooltip = d3.select('body')
                 .append('div')
                 .attr('id', 'tooltip')
                 .style('opacity', 0);
-
-            // Draw heatmap
-            const parseTime = d3.timeParse('%m');
 
             const rectWidth = Math.ceil((width - padding * 2) / (years.length / 12));
             const rectHeight = Math.ceil((height - padding * 2) / 12);
@@ -52,66 +45,43 @@ document.addEventListener('DOMContentLoaded', function () {
                 .enter()
                 .append('rect')
                 .attr('class', 'cell')
-                .attr('data-month', d => d.months)
-                .attr('data-year', d => d.years)
-                .attr('data-temp', d => d.temperatures)
+                .attr('data-month', (d, i) => d.month - 1)
+                .attr('data-year', d => d.year)
+                .attr('data-temp', d => d.variance)
                 .attr('x', (d, i) => padding + Math.floor(i / 12) * rectWidth)
-                .attr('y', (d, i) => padding + (i % 12) * rectHeight)
+                .attr('y', (d, i) => padding + (d.month - 1) * rectHeight)
                 .attr('width', rectWidth)
                 .attr('height', rectHeight)
                 .attr('fill', d => colorScale(d.variance))
 
                 .on("mouseover", function (event, d) {
-                    // Show tooltip
                     tooltip.transition()
                         .duration(200)
                         .style("opacity", .9);
-                    // Update tooltip content
-                    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-                    tooltip.html(`${d.year} - ${months[d.month - 1]} <br> Temp: ${(8.66 + d.variance).toFixed(3)}&deg;C <br> Var: ${d.variance >= 0 ? '+' : ''}${d.variance}&deg;C`);
+                    tooltip.html(`${d.year} - ${monthsArr[d.month - 1]} <br> Temp: ${(8.66 + d.variance).toFixed(3)}&deg;C <br> Var: ${d.variance >= 0 ? '+' : ''}${d.variance}&deg;C`);
                 })
                 .on("mousemove", function (event, d) {
-                    // Update tooltip position
                     tooltip.style("left", (event.pageX + 10) + "px")
                         .style("top", (event.pageY - 70) + "px")
                         .style("position", "absolute");
                 })
                 .on("mouseout", function (d) {
-                    // Hide tooltip
                     tooltip.transition()
                         .duration(500)
                         .style("opacity", 0);
                 });
 
-            // Axis
-            const xAxis = d3.axisBottom(xScale)
-                .tickFormat(d3.timeFormat('%Y'));
-            const yAxis = d3.axisLeft(yScale)
-                .tickSize(0)
-                .tickPadding(10);
-
-            svg.append('g')
-                .attr('transform', `translate(0, ${height - padding + 5})`)
-                .attr('id', 'x-axis')
-                .call(xAxis);
-
-            svg.append('text')
-                .attr('x', width / 2)
-                .attr('y', height - 10)
-                .attr('text-anchor', 'middle')
-                .text('Year');
-
+            const yAxis = d3.axisLeft(yScale);
             svg.append('g')
                 .attr('id', 'y-axis')
                 .attr('transform', `translate(${padding}, 0)`)
                 .call(yAxis);
 
-            svg.append('text')
-                .attr('x', -height / 2)
-                .attr('y', padding - 40)
-                .attr('transform', 'rotate(-90)')
-                .attr('text-anchor', 'middle')
-                .text('Months');
+            const xAxis = d3.axisBottom(xScale)
+                .tickFormat(d3.timeFormat('%Y'));
+            svg.append('g')
+                .attr('id', 'x-axis')
+                .attr('transform', `translate(0, ${height - padding + 5})`)
+                .call(xAxis);
         });
 });
